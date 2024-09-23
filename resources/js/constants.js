755c044c09e9d07657c93cef8ae5e0f35433190d
randymi01 <randymi@umich.edu>
2024-09-23 09:27:49 -0400

    added family history and tap to remove for the select fields

---
+++ resources/js/constants.js
@@ -0,0 +1,47 @@
+const hsMedications = [
+    "Clindamycin",
+    "Doxycycline",
+    "Contraceptives",
+    "Spironolactone",
+    "Acitretin",
+    "Adalimumab",
+    "Infliximab",
+    "Methotrexate",
+    "Cyclosporine",
+    "Ibuprofen",
+    "Prednisone",
+    "Acetaminophen"
+];
+const comorbidities = [
+    "Diabetes",
+    "Obesity",
+    "Hypertension",
+    "Crohn's Disease",
+    "Psoriasis",
+    "Arthritis",
+    "Asthma",
+    "Heart Disease",
+    "None"
+];
+const biologics = [
+    "Diabetes",
+    "Obesity",
+    "Hypertension",
+    "Crohn's Disease",
+    "Psoriasis",
+    "Arthritis",
+    "Asthma",
+    "Heart Disease",
+    "None"
+];
+
+const hsBiologics = [
+    "Adalimumab (Humira)",
+    "Infliximab (Remicade)",
+    "Secukinumab (Cosentyx)",
+    "Ustekinumab (Stelara)",
+    "Etanercept (Enbrel)",
+    "Tildrakizumab (Ilumya)",
+    "Guselkumab (Tremfya)",
+    "Brodalumab (Siliq)"
+];
