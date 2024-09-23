71fc84796fce69b90eb9bc3cfd21416c49d44351
randymi01 <randymi@umich.edu>
2024-09-23 10:19:16 -0400

    added comorbidities

--- resources/js/constants.js
+++ resources/js/constants.js
@@ -12,7 +12,20 @@ const hsMedications = [
     "Prednisone",
     "Acetaminophen"
 ];
+
+// https://pubmed.ncbi.nlm.nih.gov/33493574/
 const comorbidities = [
+    "Acne",
+    "PD (Pilonidal Disease)",
+    "PG (Pyoderma Gangrenosum",
+    "Herpes Zoster",
+    "Polycystic Ovary Syndrome",
+    "Dyslipidemia",
+    "MetS",
+    "Major adverse cardiovascular events",
+    "Musculoskeletal Disease",
+    "Sexual Dysfunction",
+    "Down Syndrome",
     "Diabetes",
     "Obesity",
     "Hypertension",
@@ -21,17 +34,12 @@ const comorbidities = [
     "Arthritis",
     "Asthma",
     "Heart Disease",
-    "None"
-];
-const biologics = [
-    "Diabetes",
-    "Obesity",
-    "Hypertension",
-    "Crohn's Disease",
-    "Psoriasis",
-    "Arthritis",
-    "Asthma",
-    "Heart Disease",
+    "Cancer",
+    "IBD (Inflammatory Bowel Disease)",
+    "Lupus",
+    "Depression",
+    "Epilepsy",
+    "Anxiety",
     "None"
 ];
 
